7a9533259832f81104a6d3480289710c324dd52c
Ramakant Chaudhari <chaudhariramakant754@gmail.com>
2025-06-10 16:06:23 +0530

    Add files via upload

---
+++ Digital Clock/Digital Clock.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    date = strftime('%A, %B %d, %Y')
+    label.config(text=string)
+    date_label.config(text=date)
+    label.after(1000, time)
+
+label = Label(root, font=('ds-digital', 100), background="black", foreground="cyan")
+label.pack(anchor='center')
+
+date_label = Label(root,font=('Helvetica', 54),background="black",foreground="ghostwhite",borderwidth=10,relief="ridge")
+date_label.pack()
+
+time()
+mainloop()
